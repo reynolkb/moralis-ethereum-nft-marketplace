137b00c31bfc8f204e3cf45946cb224b1cc3c128
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2022-02-11 12:35:21 -0500

    updated for rinkeby

--- src/components/SearchCollections.jsx
+++ src/components/SearchCollections.jsx
@@ -1,37 +1,37 @@
 import { Select } from 'antd';
-import { useMoralisDapp } from "providers/MoralisDappProvider/MoralisDappProvider";
-import { getCollectionsByChain } from "helpers/collections";
+import { useMoralisDapp } from 'providers/MoralisDappProvider/MoralisDappProvider';
+import { getCollectionsByChain } from 'helpers/collections';
+import React, { useState, useEffect } from 'react';
 
+function SearchCollections({ setInputValue }) {
+	const { Option } = Select;
+	const { chainId } = useMoralisDapp();
+	const NFTCollections = getCollectionsByChain(chainId);
 
-function SearchCollections({setInputValue}){
-    const { Option } = Select;
-    const { chainId } = useMoralisDapp();
-    const NFTCollections = getCollectionsByChain(chainId);
-    
-    
+	useEffect(() => {
+		// async function fetchData() {
+		// 	console.log(` ------------------- fetchMarketItems is ${JSON.stringify(fetchMarketItems)}`);
+		// 	console.log(` ----------------------- nft to buy is ${nftToBuy}`);
+		// }
+		// fetchData();
+		console.log(` ------------ NFTCollections ${NFTCollections}`);
+	}, []);
 
-    function onChange(value) {
-        setInputValue(value);
-    }
+	function onChange(value) {
+		setInputValue(value);
+	}
 
-    return (
-        <>
-        <Select
-            showSearch
-            style={{width: "1000px",
-                    marginLeft: "20px" }}
-            placeholder="Find a Collection"
-            optionFilterProp="children"
-            onChange={onChange}
-        >   
-        {NFTCollections && 
-            NFTCollections.map((collection, i) => 
-            <Option value={collection.addrs} key= {i}>{collection.name}</Option>
-            )
-            }   
-        </Select>
-            
-        </>
-    )
+	return (
+		<>
+			<Select showSearch style={{ width: '1000px', marginLeft: '20px' }} placeholder='Find a Collection' optionFilterProp='children' onChange={onChange}>
+				{NFTCollections &&
+					NFTCollections.map((collection, i) => (
+						<Option value={collection.addrs} key={i}>
+							{collection.name}
+						</Option>
+					))}
+			</Select>
+		</>
+	);
 }
 export default SearchCollections;
